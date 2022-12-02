9fe9bbd1c6e9a91b5af2b1b2278be3d2468b600d
Vinty666 <43361479+Vinty666@users.noreply.github.com>
2022-12-01 23:29:36 -0500

    To solve merge conflicts

--- server/server.js
+++ server/server.js
@@ -14,6 +14,30 @@ app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 // Route 1 - register as GET 
 app.get('/hello', routes.hello)
 
+//Query 1
+app.get('/search/artists/searchArtist/',routes.search_artist)
+//Query 2
+app.get('/search/artist/collaborators/',routes.search_collaborators)
+
+// Query 6
+app.get('/search/artists/:followers', routes.searchArtistsWithFollowers)
+
+// Query 7
+app.get('/search/artists/:followers/:popularity/:numSongs', routes.searchArtistsWithPopularitySongs)
+
+// Query 8
+app.get('/search/artists/grammy/:yearDiff', routes.searchArtistsGrammyWithTimeDiff)
+
+// Query 5 - register as GET
+app.get('/search/specificSongs/:year', routes.search_specific_songs)
+
+// Query 3 - register as GET
+app.get('/search/bfsCoCooperator/', routes.search_co_cooperator)
+
+// Query 4 - register as GET
+app.get('/search/topSong/:genre',routes.search_top_songs)
+//Route
+
 // Query 9
 app.get('/search/album/genre', routes.grammyAlbumsWithinTime)
 
