693ac6360e411ef8108da5d4957d6a0ead78b125
3054405953@qq.com <3054405953@qq.com>
2022-12-07 18:39:07 -0500

    update fetch (untested)

--- server/client/src/fetcher.js
+++ server/client/src/fetcher.js
@@ -1,62 +1,67 @@
 import config from './config.json'
 
-const getAllMatches = async (page, pagesize, league) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
-        method: 'GET',
-    })
-    return res.json()
+
+//Hanlin Added here
+//Just for testing
+const searchArtist=async(genre,certainYear,weeks,albumThreshold)=>{
+var res=await fetch(`http://${config.server.host}:${config.server_port}/getArtist?genre=${genre}&certainYear=${certainYear}&weeks=${weeks}&albumThreshold=${albumThreshold}`,{
+    method:'GET'
+})
 }
 
-const getAllPlayers = async (page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/players?page=${page}&pagesize=${pagesize}`, {
-        method: 'GET',
+const searchCollaborators=async(artist,popThreshold,folThreshold)=>{
+    var res=await fetch(`http://${config.server.host}:${config.server_port}/getArtist/cooperators?artist=${artist}&popThreshold=${popThreshold}&folThreshold=${folThreshold}`,{
+        method:'GET'
     })
-    return res.json()
-}
-
-const getMatch = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/match?id=${id}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
-
-const getPlayer = async (id) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
-
-const getMatchSearch = async (home, away, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
-        method: 'GET',
-    })
-    return res.json()
-}
-
-const getPlayerSearch = async (name, nationality, club, rating_high, rating_low, pot_high, pot_low, page, pagesize) => {
-    var res = await fetch(`http://${config.server_host}:${config.server_port}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
-        method: 'GET',
-    })
-    return res.json()
 }
 
 
-
-
-
-
-
-
-
-
-
-
+// //Reference
+// const getAllMatches = async (page, pagesize, league) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/matches/${league}?page=${page}&pagesize=${pagesize}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
+//
+// const getAllPlayers = async (page, pagesize) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/players?page=${page}&pagesize=${pagesize}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
+//
+// const getMatch = async (id) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/match?id=${id}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
+//
+// const getPlayer = async (id) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/player?id=${id}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
+//
+// const getMatchSearch = async (home, away, page, pagesize) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/search/matches?Home=${home}&Away=${away}&page=${page}&pagesize=${pagesize}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
+//
+// const getPlayerSearch = async (name, nationality, club, rating_high, rating_low, pot_high, pot_low, page, pagesize) => {
+//     var res = await fetch(`http://${config.server_host}:${config.server_port}/search/players?Name=${name}&Nationality=${nationality}&Club=${club}&RatingLow=${rating_low}&RatingHigh=${rating_high}&PotentialHigh=${pot_high}&PotentialLow=${pot_low}&page=${page}&pagesize=${pagesize}`, {
+//         method: 'GET',
+//     })
+//     return res.json()
+// }
 
 export {
-    getAllMatches,
-    getAllPlayers,
+    searchArtist,
+    searchCollaborators,
     getMatch,
     getPlayer,
     getMatchSearch,
