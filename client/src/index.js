7303c15f50b10b8113834fcf3a6c6905a2018236
Vinty666 <43361479+Vinty666@users.noreply.github.com>
2022-12-13 04:41:28 -0500

    remove unnecessary page import in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,19 +8,15 @@ import {
 
 import HomePage from './pages/HomePage';
 import ArtistPage from './pages/ArtistPage';
-
-import SearchSongPage from './pages/SearchSongPage';
-import ArtistDetailsPage from './pages/ArtistDetails';
-import SongDetailsPage from './pages/SongDetails';
-import ArtistPage from './pages/ArtistPage';
 import SongPage from './pages/SongPage'
 
-import TopSongPage from './pages/TopSongPage';
+import ArtistDetailsPage from './pages/ArtistDetails';
+import SongDetailsPage from './pages/SongDetails';
+
 import 'antd/dist/antd.css';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
-import MatchesPage from './pages/MatchesPage';
 
 ReactDOM.render(
   <div>
@@ -40,16 +36,6 @@ ReactDOM.render(
 							path="/songs"
 							render={() => (
 								<SongPage />
-							)}/>
-        <Route exact
-							path="/search_song"
-							render={() => (
-								<SearchSongPage />
-							)}/>
-        <Route exact
-							path="/top_song"
-							render={() => (
-								<TopSongPage />
 							)}/>
 		<Route exact 
 							path="/artist_details"
